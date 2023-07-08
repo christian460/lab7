bfb2babc438f30b25e01796ac19f9e665d8758f3
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-08 12:41:23 -0500

    Renderizar y descargar los pdf

--- model_examples/model_examples/views.py
+++ model_examples/model_examples/views.py
@@ -8,10 +8,20 @@ class GeneratePdf(View):
     def get(self, request, *args, **kwargs):
         template = get_template('invoice.html')
         context = {
-            "invoce_id": 123, 
+            "invoice_id": 123, 
             "customer_name": "John Cooper",
             "amount": 1399.99,
             "today": "Today",
         }
         html = template.render(context)
-        return HttpResponse(html)
+        pdf = render_to_pdf('invoice.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" %("12341231")
+            content = "inline; filname=%s" %(filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename=%s" %(filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not Found")

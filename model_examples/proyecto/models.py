b98838de3058ad8799c7cdb7598c1331a146dce9
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-10 23:27:41 -0500

    Se creo en el modelo la clase proyecto y autor

--- model_examples/proyecto/models.py
+++ model_examples/proyecto/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    name=models.CharField(max_length=20)
+    cargo=models.CharField(max_length=20)
+    perfil=models.ImageField(upload_to='pics')
+    def __str__(self):
+        return self.name
+    
+class Proyecto(models.Model):
+    name=models.CharField(max_length=20)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+    autor=models.ForeignKey(Autor, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+
+
+    

21c11b9bc67ca6a058b2512697b4d2594911a058
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-10 23:25:19 -0500

    Se personalizo el mensaje

--- model_examples/correo/views.py
+++ model_examples/correo/views.py
@@ -5,10 +5,11 @@ from django.core.mail import send_mail
 # Create your views here.
 
 def enviar(request):
-    send_mail('Un email desde django', 
-    'Hola te escribimos desde django',
+    send_mail('Aria: '+request.POST['lselect'],
+    'Hola '+request.POST['lname']+', te hablamos de Aria te agredecemos que estes interesados en nuestro proyecto. En breve te contactaremos al numero '
+    +request.POST['lphone'],
     settings.EMAIL_HOST_USER,
-    ['juego4661@gmail.com'],
+    [request.POST['lemail']],
     fail_silently=False)
     
     return render(request,'enviar.html')

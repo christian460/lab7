62ed03e0a86dc873331a7e180ea01c98aa087c1b
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-10 21:18:40 -0500

    solucion de errores

--- model_examples/model_examples/urls.py
+++ model_examples/model_examples/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import GeneratePdf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pdf/',GeneratePdf.as_view())
+    path('pdf/',GeneratePdf.as_view()),
+    path('send/', include('correo.urls'))
 ]

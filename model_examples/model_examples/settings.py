4daa54efd2565c585033b260ec66f20f8773c8f0
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-10 21:15:57 -0500

    se configuro la aplcacion para poder enviar correos

--- model_examples/model_examples/settings.py
+++ model_examples/model_examples/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example'
+    'example',
+    'correo'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'verdoso64@gmail.com'
+EMAIL_HOST_PASSWORD = 'swqleuvupjoubzjr'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+

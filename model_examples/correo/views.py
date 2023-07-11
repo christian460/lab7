a1c5972f50b5899a6e1ad847947df6c69fc71dc5
Pedro Luis Christian Zapana Romero <pzapanar@unsa.edu.pe>
2023-07-10 21:14:57 -0500

    Se creo el metodo para enviar correos

--- model_examples/correo/views.py
+++ model_examples/correo/views.py
@@ -1,3 +1,14 @@
+from django.conf import settings 
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
+
+def enviar(request):
+    send_mail('Un email desde django', 
+    'Hola te escribimos desde django',
+    settings.EMAIL_HOST_USER,
+    ['juego4661@gmail.com'],
+    fail_silently=False)
+    
+    return render(request,'enviar.html')
